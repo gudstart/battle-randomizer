ea3c0a511ddea368fbc8fcd427878edb421bf585
Ning Hu <hn1105@gmail.com>
2023-11-08 00:02:34 -0500

    switching laptops

--- battle-randomizer.py
+++ battle-randomizer.py
@@ -2,47 +2,55 @@ import random as rand
 import numpy as np
 import string
 
-#### RULES ####
+randin = np.random.randint
+print(randin(50))
+#### SETTINGS ####
 NUM_RULES = 3
+NUM_MONS = 3
+TIME_LIMIT = 7
+RULE_MAX_BP_RANGE = [50, 130]
+RULE_MAX_STAT_RANGE = [40, 100]
+
+SETTING_NATDEX_CHANCE = 50
+SETTING_TERASTALLIZE_CHANCE = 50
 
 rules = [
-    #(Rule, Weight, Included)
-    ("Only mons starting with @", 10, True),
-    ("Only moves starting with @", 10, True),
-    ("Only items starting with @", 10, True),
-    ("Only mons that have 1 type", 10, True),
-    ("Only mons that are part & type", 10, True),
-    ("Damaging moves can only be up to # base power", 10, True)
-    ("No STAB moves allowed (Tera type included if terastallized)", 10, True)
-    ("* stat has to be between < and >", 10, True)
+    {"Only mons starting with @": 10},
+    {"Only moves starting with @": 10},
+    {"Only items starting with @": 10},
+    {
+        "Only mons that have 1 type": 5,
+        "Only mons that are part & type": 10
+    },
+    {"Damaging moves can only be up to # base power": 10},
+    {"No STAB moves allowed (Tera type included if terastallized)": 0},
+    {"* stat has to be max !": 10}
 ]
 
 #LISTS
 types = ["Normal", "Fire", "Water", "Grass", "Electric", "Ice", "Fighting", "Poison", 
          "Ground", "Flying", "Psychic", "Bug", "Rock", "Ghost", "Dragon", "Dark", "Steel", "Fairy"]
 tiers = ["LC", "ZU", "NU", "PU", "RU", "UU", "OU", "Ubers"]
+stats = ["HP", "Attack", "Defense", "Sp. Atk", "Sp. Def", "Speed"]
 
 #### CLAUSES ####
-def getRandom(n):
-    return np.random.randint(n)
-
-print("3 MONS, 7 MINUTE TIME LIMIT")
-print("NATIONAL DEX")         if getRandom(100) <= 50 else print("SV POKEDEX")            # Dex Picker
+print(NUM_MONS, " MONS, ", TIME_LIMIT," MINUTE TIME LIMIT")
+print("NATIONAL DEX")         if randint(100) <= NAT else print("SV POKEDEX")            # Dex Picker
 print("Only", rand.choice(tiers), "pokemon or below")                                # Tier Picker
-print("TERASTALLIZE ON") if getRandom(100) <= 50 else print("TERASTALLIZE OFF")
+print("TERASTALLIZE ON") if randint(100) <=  else print("TERASTALLIZE OFF")
 print()
 
-print("Species Clause ON")    if getRandom(100) <= 90 else print("Species Clause OFF")    # Species Clause
-print("Moody Clause ON")      if getRandom(100) <= 70 else print("Moody Clause OFF")    # Moody Clause
-print("OHKO Clause ON")       if getRandom(100) <= 85 else print("OHKO Clause OFF")    # OHKO Clause
-print("Evasion Clause ON")    if getRandom(100) <= 85 else print("Evasion Clause OFF")    # Evasion Clause
-if getRandom(100) <= 40: print("Item Clause ON (No 2 pokemon may have the same item)")    # Item Clause
+print("Species Clause ON")    if randint(100) <= 90 else print("Species Clause OFF")    # Species Clause
+print("Moody Clause ON")      if randint(100) <= 70 else print("Moody Clause OFF")    # Moody Clause
+print("OHKO Clause ON")       if randint(100) <= 85 else print("OHKO Clause OFF")    # OHKO Clause
+print("Evasion Clause ON")    if randint(100) <= 85 else print("Evasion Clause OFF")    # Evasion Clause
+if randint(100) > 50: print("Item Clause ON (No 2 pokemon may have the same item)")    # Item Clause
 print()
 
 #### SELECTED RULES ####
 for i in range(NUM_RULES):
-    selectedRules = [(rule, weight, selected) for rule, weight, selected in rules if selected == True]
-    randNum = np.random.randint(np.sum([t[1] for t in selectedRules]))
+    selectedRules = [(key value) for rule, weight, selected in rules if selected == True]
+    randNum = randint(np.sum([t[1] for t in selectedRules]))
 
     selectedRule = 0
     for t in selectedRules:
@@ -56,5 +64,9 @@ for i in range(NUM_RULES):
     selectedRuleString = selectedRuleString.replace("&", rand.choice(types))
     bpLowerBound = 50
     bpUpperBound = 130
-    selectedRuleString = selectedRuleString.replace("#", str(np.random.randint(bpLowerBound/5, bpUpperBound/5)*5))
+    selectedRuleString = selectedRuleString.replace("#", str(randint(bpLowerBound/5, bpUpperBound/5)*5))
+    statLowerBound = 40
+    statUpperBound = 110
+    selectedRuleString = selectedRuleString.replace("*", rand.choice(stats))
+    selectedRuleString = selectedRuleString.replace("!", str(randint(statLowerBound/5, statUpperBound/5)*5))
     print("RULE " + str(i + 1) + ": " + selectedRuleString)
