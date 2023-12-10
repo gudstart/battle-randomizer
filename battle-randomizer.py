43d11e05abbf7380675c3adee238a6d9346cf6ac
Ning Hu <hn1105@gmail.com>
2023-12-09 19:25:36 -0500

    huge update

--- battle-randomizer.py
+++ battle-randomizer.py
@@ -1,3 +1,4 @@
+import re
 import random as rand
 import string
 import time
@@ -22,6 +23,7 @@ MOODY_CLAUSE_ON_CHANCE = 60
 OHKO_CLAUSE_ON_CHANCE = 85
 EVASION_CLAUSE_ON_CHANCE = 80
 ITEM_CLAUSE_ON_CHANCE = 50
+TRAPPING_CLAUSE_ON_CHANCE = 70
 
 #### RULESET ####
 # List of dictionaries representing categories of rules.
@@ -29,36 +31,54 @@ ITEM_CLAUSE_ON_CHANCE = 50
 # If a rule from a category is selected, the other rules from that category can no longer be selected for further rules.
 
 ruleset = [
-    {"Only mons starting with **@**": 20},
+    
+    #POKEMON RULES
+    {
+        "Only mons starting with **@**": 10,
+        "Only mons that have the following abilities: **^**, **^** or **^** (Their other abilities may also be used)": 10,
+        "Only mons with less than an 8 letter name": 3,
+        "Only mons with an 8 or more letter name": 3,
+        "Only mons that have **1** type": 5,
+        "Only mons that are part **&** type": 10
+    },
+    
+    #MOVE RULES
     {
         "Only moves starting with **@**": 14,
         "Only moves that have 1 word": 3,
         "Only moves that have 2 words": 3
     },
+    
+    #ITEM RULES
     {
-        "Only items starting with **@**": 11,
-        "Only **gems** allowed as items (Use natdex format for battle)": 3,
-        "Only **berries** allowed as items": 3,
-        "Only **plates** allowed as items": 3
+        "Only items starting with **@**": 5,
+        "Only **gems** allowed as items": 5,
+        "Only **berries** allowed as items": 5,
+        "Only **plates** allowed as items": 5,
+        "Only **Z items** allowed as items": 5
     },
-    {
-        "Only mons that have **1** type": 5,
-        "Only mons that are part **&** type": 15
-    },
-    {"Damaging moves can only be up to **#** base power (Multiply minimum number of hits for multihit moves)": 10},
+    
+    #HACKMON RULES
+    {"Regardless of other rules, all mons have access to the move **$**": 15},
+    {"Regardless of other rules, all mons have access to any move that starts with **@**": 5},
+    {"Regardless of other rules, all mons have access to the ability **^**": 15},
+    {"Regardless of other rules, all mons have access to any ability that starts with **@**": 5},
+    {"All mons may use up to 1 of ANY existing **&** type move including their own": 15},
+    
+    #NATURE RULES
+    {"Your mons must have the **%** nature": 5},
+    
+    #OTHER RULES
+    {"Damaging moves can only be up to **#** base power. Multihit moves can only be up to **`** base power.": 10},
     {"No STAB moves allowed (Tera type included if terastallized)": 2},
-    {
-        "Maximum base | stat is **!**": 10,
-    },
-    {"Regardless of other rules, all mons have access to the move **$** (Use custom game for battle)": 20},
-    {"Regardless of other rules, all mons have access to the ability **^** (Use custom game for battle)": 20},
-    {"Your mons must have the **%** nature": 5}
+    {"All mons must not surpass a base | stat of **!**": 10}
 ]
 
 #LISTS
 types = ["Normal", "Fire", "Water", "Grass", "Electric", "Ice", "Fighting", "Poison", 
          "Ground", "Flying", "Psychic", "Bug", "Rock", "Ghost", "Dragon", "Dark", "Steel", "Fairy"]
-tiers = ["LC", "ZU", "NU", "PU", "RU", "UU", "OU", "Ubers"]
+SV_tiers = ["LC", "ZU", "NU", "PU", "RU", "UU", "OU", "Ubers"]
+NATDEX_tiers = ["LC", "PFE", "RU", "UU", "OU", "Ubers"]
 stats = ["HP", "Attack", "Defense", "Sp. Atk", "Sp. Def", "Speed"]
 influential_natures = ["Lonely", "Adamant", "Naughty", "Brave", "Bold", "Impish", "Lax", "Relaxed", "Modest", "Bashful", "Rash", "Quiet", "Calm", "Gentle", "Careful", "Sassy", "Timid", "Hasty", "Jolly", "Naive"]
 numMoves = 904 #We use pokebase api for moves
@@ -67,19 +87,24 @@ numAbilities = 303 #We use pokebase api for abilities
 def getBattleRules():
     global ruleset
     rules = ruleset
-    battleRules = "----------------------------------------------------------------------------\n"
     
     #### CLAUSES ####
-    battleRules += "## " + str(NUM_MONS) + " MONS, " + str(TIME_LIMIT_MINUTES) + " MINUTE TIME LIMIT\n"
-    battleRules += "**NATIONAL DEX**\n"         if randint(100) <= SETTING_NATDEX_CHANCE else "**SV POKEDEX**\n"
-    battleRules += "Only **" + rand.choice(tiers) + "** pokemon or below\n"
+    battleRules = "## " + str(NUM_MONS) + " MONS, " + str(TIME_LIMIT_MINUTES) + " MINUTE TIME LIMIT\n"
+    if randint(100) <= SETTING_NATDEX_CHANCE:
+        battleRules += "**NATIONAL DEX**\n"
+        battleRules += "Only **" + rand.choice(NATDEX_tiers) + "** pokemon or below\n"
+    else:
+        battleRules += "**SV DEX**\n"
+        battleRules += "Only **" + rand.choice(SV_tiers) + "** pokemon or below\n"
+    
     battleRules += "TERASTALLIZE **ON**\n\n" if randint(100) <= SETTING_TERASTALLIZE_ON_CHANCE else "TERASTALLIZE **OFF**\n\n"
 
-    battleRules += "Species Clause ON\n"    if randint(100) <= SPECIES_CLAUSE_ON_CHANCE else "Species Clause **OFF** (More than 1 of the same species may be used)\n"   # Species Clause
-    battleRules += "Moody Clause ON\n"      if randint(100) <= MOODY_CLAUSE_ON_CHANCE else "Moody Clause **OFF** (The ability Moody is allowed)\n"    # Moody Clause
-    battleRules += "OHKO Clause ON\n"       if randint(100) <= OHKO_CLAUSE_ON_CHANCE else "OHKO Clause **OFF** (OHKO moves are allowed)\n"    # OHKO Clause
-    battleRules += "Evasion Clause ON\n"    if randint(100) <= EVASION_CLAUSE_ON_CHANCE else "Evasion Clause **OFF** (Evasion moves are allowed)\n"    # Evasion Clause
-    if randint(100) <= ITEM_CLAUSE_ON_CHANCE: battleRules += "Item Clause **ON** (No 2 pokemon may have the same item)\n"    # Item Clause
+    battleRules += "Species Clause ON\n"    if randint(100) <= SPECIES_CLAUSE_ON_CHANCE else "Species Clause **OFF** (More than 1 of the same species may be used)\n"
+    battleRules += "Moody Clause ON\n"      if randint(100) <= MOODY_CLAUSE_ON_CHANCE else "Moody Clause **OFF** (The ability Moody is allowed)\n"
+    battleRules += "OHKO Clause ON\n"       if randint(100) <= OHKO_CLAUSE_ON_CHANCE else "OHKO Clause **OFF** (OHKO moves are allowed)\n"
+    battleRules += "Evasion Clause ON\n"    if randint(100) <= EVASION_CLAUSE_ON_CHANCE else "Evasion Clause **OFF** (Evasion moves are allowed)\n"
+    battleRules += "Trapping Clause ON\n"    if randint(100) <= TRAPPING_CLAUSE_ON_CHANCE else "Trapping Clause **OFF** (Trapping moves and abilities are allowed)\n"
+    if randint(100) <= ITEM_CLAUSE_ON_CHANCE: battleRules += "Item Clause **ON** (No 2 pokemon may have the same item)\n"
     battleRules += "\n"
 
     #### SELECTED RULES ####
@@ -97,7 +122,8 @@ def getBattleRules():
 
         selectedRuleString = selectedRule[0].replace("@", rand.choice(string.ascii_letters).upper())
         selectedRuleString = selectedRuleString.replace("&", rand.choice(types))
-        selectedRuleString = selectedRuleString.replace("#", str(randint(RULE_MAX_BP_RANGE[0]/5, RULE_MAX_BP_RANGE[1]/5)*5))
+        bpRuleNum = randint(RULE_MAX_BP_RANGE[0]/5, RULE_MAX_BP_RANGE[1]/5)*5
+        selectedRuleString = selectedRuleString.replace("#", str(bpRuleNum)).replace("`", str(int(round(bpRuleNum/5+10))))
         selectedRuleString = selectedRuleString.replace("|", rand.choice(stats))
         selectedRuleString = selectedRuleString.replace("!", str(randint(RULE_MAX_STAT_RANGE[0]/5, RULE_MAX_STAT_RANGE[1]/5)*5))
         if selectedRuleString.find("$"): #Minimize need for API call
@@ -105,7 +131,7 @@ def getBattleRules():
         selectedRuleString = selectedRuleString.replace("%", rand.choice(influential_natures))
         selectedRuleString = selectedRuleString.replace("?", str(randint(RULE_MAX_BST_RANGE[0], RULE_MAX_BST_RANGE[1])))
         if selectedRuleString.find("^"): #Minimize need for API call
-            selectedRuleString = selectedRuleString.replace("^", str(pb.ability(randint(1, numAbilities))).replace("-", " ").title())
+            selectedRuleString = re.sub(r'\^', lambda x: str(pb.ability(randint(1, numAbilities))).replace("-", " ").title(), selectedRuleString)
         battleRules += "- " + selectedRuleString + "\n"
     
     battleRules += "[Teambuilder](https://play.pokemonshowdown.com/teambuilder)\n"
